e7f216f785ef877233cd52c4999d48da299991ad
d36choi <yuio1126@gmail.com>
2023-12-16 17:17:41 +0900

    feat: shop controller mock data 추가

--- src/main/java/io/api/lottoboard/controller/ShopApiController.java
+++ src/main/java/io/api/lottoboard/controller/ShopApiController.java
@@ -3,10 +3,14 @@ package io.api.lottoboard.controller;
 import io.api.lottoboard.dto.ShopDto;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @RequiredArgsConstructor
 @Getter
@@ -15,8 +19,9 @@ public class ShopApiController {
 
 
     @GetMapping("/shop")
-    public List<ShopDto.Response> getShops() {
-
-        return List.of(new ShopDto.Response());
+    public List<ShopDto.Response> getShops(Pageable pageable) {
+        return IntStream.range(0, pageable.getPageSize())
+                .mapToObj(ShopDto.Response::mockOf)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/io/api/lottoboard/dto/ShopDto.java
+++ src/main/java/io/api/lottoboard/dto/ShopDto.java
@@ -1,16 +1,22 @@
 package io.api.lottoboard.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 public class ShopDto {
 
     @NoArgsConstructor
+    @AllArgsConstructor
     @Getter
     public static class Response {
         private String name;
         private String location;
-        private Long firstPrizeCount;
-        private Long secondPrizeCount;
+        private long firstPrizeCount;
+        private long secondPrizeCount;
+
+        public static ShopDto.Response mockOf(int i) {
+            return new Response("test", "test location", i, i);
+        }
     }
 }
